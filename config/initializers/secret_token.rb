305d810173f11de33fd48d6bb1ce6117e449623b
Jeremy Hageman <jjhageman@gmail.com>
2013-05-11 12:41:51 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conquer2::Application.config.secret_token = 'edace4daf7c5c2adb640a326b603a6b3b6726eb42588bcb73d64fcac84962f25dc87f2bea98baf2398d2dc68ba2eefe925cfcf494d30ac25132d58cfdb6acd49'
