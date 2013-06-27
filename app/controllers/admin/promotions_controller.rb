a4c6feb714c285e5b22abd919521e36f877ab373
Jeremy Hageman <jhageman@affinitylabs.com>
2013-06-26 17:33:59 -0700

    admin promo crud

---
+++ app/controllers/admin/promotions_controller.rb
@@ -0,0 +1,17 @@
+module Admin
+  class PromotionsController < BaseController
+    before_filter :load_course
+
+    def show
+      @promotion = @course.promotions.find_by_code(params[:id]) 
+    end
+
+    private
+
+    def load_course
+      unless @course = Course.find_by_url(params[:course_id])
+        redirect_to user_root_path, alert: 'Invalid course'
+      end
+    end
+  end
+end
