2f329b5581e21e5fac2690e2532518c889091cf7
jjhageman <jjhageman@gmail.com>
2013-08-04 18:31:51 -0700

    admin course update

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -24,5 +24,15 @@ module Admin
     def edit
       @course = Course.find_by_url(params[:id])
     end
+
+    def update
+      @course = Course.find(params[:id])
+    
+      if @course.update_attributes(params[:course])
+        redirect_to admin_course_path(@course), notice: 'Chapter was successfully updated.'
+      else
+        render 'edit'
+      end
+    end
   end
 end
